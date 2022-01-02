cbf4f9b0b7ee96973f00caf5202c1044b27b2cec
Ukpeh Michael <marcelukpeh@gmail.com>
2022-01-02 16:59:47 +0100

    Test commit

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,11 +1,29 @@
 import Layout from '../../components/Layout'
+import {useRouter} from 'next/router';
+import { useEffect, useState } from 'react';
+
 const Dashboard = () => {
-    return (
-<div>
-    Hello Bitch
-</div>
-    )
+  const [user, setUser] = useState([])
+  const router = useRouter ();
+  const {username} = router.query;
+  console.log (router.query.username);
+  useEffect(() => {
+      async() =>{
+    const res = await fetch(`https://api.github.com/users/${username}`)
+    const user = res.json()
+    .then((result) => setUser(result))
+    .catch(error => console.log(error))
+      }
+  }, [username])
+
+  console.log(user)
+  return (
+    <div>
+      hello {user.name}
+    </div>
+  )
 }
+
 
 export default Dashboard
 
